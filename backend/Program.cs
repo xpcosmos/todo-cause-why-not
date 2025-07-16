ed7ced208ebf67144e3966213ef363799dd4f1ba
xpcosmos <mikeias.d.s.o@gmail.com>
2025-07-16 20:21:51 -0300

    SETUP: Backend

---
+++ backend/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+app.Run();
