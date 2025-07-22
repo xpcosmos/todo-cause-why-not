49bfc7874102fcf064e8c85471680e852696d55e
xpcosmos <mikeias.d.s.o@gmail.com>
2025-07-22 14:02:10 -0300

    build(spring): Adicionando spring boot como backend framework

---
+++ src/main/java/com/xpcosmos/todo_cause_why_not/TodoCauseWhyNotApplication.java
@@ -0,0 +1,13 @@
+package com.xpcosmos.todo_cause_why_not;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoCauseWhyNotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoCauseWhyNotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xpcosmos/todo_cause_why_not/TodoCauseWhyNotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpcosmos.todo_cause_why_not;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoCauseWhyNotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
