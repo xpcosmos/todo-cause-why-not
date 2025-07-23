1f86709bf20990fdc5121355ed384db493acdd08
xpcosmos <mikeias.d.s.o@gmail.com>
2025-07-23 14:12:30 -0300

    feat: Adição de Getters e Setters

--- src/main/java/com/xpcosmos/todo_cause_why_not/todo/Todo.java
+++ src/main/java/com/xpcosmos/todo_cause_why_not/todo/Todo.java
@@ -8,12 +8,51 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document("Todo")
 public class Todo {
-  
+
   @Id
   private ObjectId id;
   private String taskTitle;
   private String taskDescription;
   private LocalDateTime expectedCompletion;
   private LocalDateTime startDateTime;
+
+  public Todo() {
+  };
+
+  public ObjectId getId() {
+    return this.id;
+  };
+
+  public String getTaskTitle() {
+    return this.taskTitle;
+  }
+
+  public void setTaskTitle(String value) {
+    this.taskTitle = value;
+  }
+
+  public String getTaskDescription() {
+    return this.taskDescription;
+  }
+
+  public void setTaskDescription(String value) {
+    this.taskDescription = value;
+  }
+
+  public LocalDateTime getExpectedCompletion() {
+    return this.expectedCompletion;
+  }
+
+  public void setExpectedCompletion(LocalDateTime value) {
+    this.expectedCompletion = value;
+  }
+
+  public LocalDateTime getStartDateTime() {
+    return this.startDateTime;
+  }
+
+  public void setStartDateTime(LocalDateTime value) {
+    this.startDateTime = value;
+  }
 
 }
