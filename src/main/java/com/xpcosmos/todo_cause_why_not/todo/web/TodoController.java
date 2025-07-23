05bd0b5fdc955bc87b21447e99ff95c44b02b7bc
xpcosmos <mikeias.d.s.o@gmail.com>
2025-07-23 14:44:10 -0300

    feat(api): Implementação de Metodos GET e POST para todo

---
+++ src/main/java/com/xpcosmos/todo_cause_why_not/todo/web/TodoController.java
@@ -0,0 +1,31 @@
+package com.xpcosmos.todo_cause_why_not.todo.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.xpcosmos.todo_cause_why_not.todo.Todo;
+import com.xpcosmos.todo_cause_why_not.todo.TodoRepository;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class TodoController {
+  
+  @Autowired TodoRepository todoRepository;
+
+  @GetMapping("/todos")
+  public List<Todo> getAll() {
+      return todoRepository.findAll();
+  }
+
+  @PostMapping("/todos")
+  public void createTodo(@RequestBody Todo todo) {
+      todoRepository.insert(todo);
+  }
+
+}
