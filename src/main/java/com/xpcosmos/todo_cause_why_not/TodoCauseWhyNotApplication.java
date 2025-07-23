3b10d58346ee7791b99284c788e2f5fe8ff66e04
xpcosmos <mikeias.d.s.o@gmail.com>
2025-07-23 14:26:20 -0300

    feat: Adicionado Decorator para habilitar Mongo Repo a aplicação

--- src/main/java/com/xpcosmos/todo_cause_why_not/TodoCauseWhyNotApplication.java
+++ src/main/java/com/xpcosmos/todo_cause_why_not/TodoCauseWhyNotApplication.java
@@ -2,8 +2,12 @@ package com.xpcosmos.todo_cause_why_not;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.xpcosmos.todo_cause_why_not.todo.TodoRepository;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackageClasses = TodoRepository.class)
 public class TodoCauseWhyNotApplication {
 
 	public static void main(String[] args) {
