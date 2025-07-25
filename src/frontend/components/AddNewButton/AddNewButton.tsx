edb992eb95da825340b30213630bd170cfb77c75
xpcosmos <mikeias.d.s.o@gmail.com>
2025-07-25 15:33:12 -0300

    feat: Adicionado animação ao posicionamento do botao

--- src/frontend/components/AddNewButton/AddNewButton.tsx
+++ src/frontend/components/AddNewButton/AddNewButton.tsx
@@ -9,19 +9,25 @@ const AddNewButton = () => {
   const [isFormOpen, setIsFormOpen]  = useState<boolean>(false);
 
 
-  const to : TargetAndTransition = {
+  const toDiv : TargetAndTransition = {
     top:"50%",
     left:  "50%",
     borderRadius: "10px",
     width: "60vw",
     height: "80vh",
-    
+  }
+  const toButton : TargetAndTransition = {
+    top:"3%",
+    left: "93%"
   }
 
   return (
     <>
-    <motion.div className='add-new' animate={isFormOpen ? to : {}} >
-      <button onClick={() => setIsFormOpen(e => !e)} >.</button>
+    <motion.div className='add-new' animate={isFormOpen ? toDiv : {}}>
+      <motion.button animate = {isFormOpen? toButton : {}} className="add-new-button" onClick={() => setIsFormOpen(e => !e)} >
+        x
+      </motion.button>
+  
     </motion.div>
     
     
