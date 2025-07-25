fc329bb52315f9230e8f06b31a79e51ffd30f33c
xpcosmos <mikeias.d.s.o@gmail.com>
2025-07-25 15:24:05 -0300

    feat: Adicionado Animacao ao botao para adicionar todo

--- src/frontend/components/AddNewButton/AddNewButton.tsx
+++ src/frontend/components/AddNewButton/AddNewButton.tsx
@@ -1,10 +1,30 @@
-import "./AddNewButton.scss"
+
+import { motion, type TargetAndTransition } from "motion/react"
+import  "./AddNewButton.scss"
+import { useState } from "react"
+
+
 const AddNewButton = () => {
+
+  const [isFormOpen, setIsFormOpen]  = useState<boolean>(false);
+
+
+  const to : TargetAndTransition = {
+    top:"50%",
+    left:  "50%",
+    borderRadius: "10px",
+    width: "60vw",
+    height: "80vh",
+    
+  }
+
   return (
     <>
-    <div className='add-new'>
-      <button>Add New</button>
-    </div>
+    <motion.div className='add-new' animate={isFormOpen ? to : {}} >
+      <button onClick={() => setIsFormOpen(e => !e)} >.</button>
+    </motion.div>
+    
+    
     </>
   )
 }
